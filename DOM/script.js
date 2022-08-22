f04f187eeb5f15418adc9f9049bc2c5e9d83d4b1
Juan Carlos Gomez Jaramillo <60585685+juangomez88@users.noreply.github.com>
2022-08-22 15:40:08 -0500

    Agregra .gitignore y primeros archivos del proyecto

---
+++ DOM/script.js
@@ -0,0 +1,18 @@
+
+
+
+const title = document.querySelector('h1');
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+
+const sumarInputValues = (event) => {
+    event.preventDefault();
+    const sumaInputs = parseInt(input1.value) + parseInt(input2.value); 
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
+
+form.addEventListener('submit', sumarInputValues);
